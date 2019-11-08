f4dbe3b362d5ad6e987645313aac33f9ba4f748b
Mariia Ginzburg <mariia.ginzburg@valtech.de>
2019-11-08 15:42:32 +0100

    initial commit

---
+++ src/main/java/de/michalush/graphqlplayground/GraphqlPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package de.michalush.graphqlplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/michalush/graphqlplayground/GraphqlPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package de.michalush.graphqlplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
